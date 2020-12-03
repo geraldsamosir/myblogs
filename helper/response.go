12d09925755b515277cdf50574c900bca2662e8d
Gerald Halomoan Samosir <gerald.unforgiven@gmail.com>
2020-12-03 16:54:01 +0700

    Starter (#6)
    * add depdencies
    
    * add depedency
    
    * add depedency
    
    * done webserver and database connection
    
    * done skeleton
    
    * done skeleton
    
    * add response helper

--- helper/response.go
+++ helper/response.go
@@ -1,5 +1,16 @@
 package helper
 
-func Response(status int, message string, response interface{}) {
+import (
+	echo "github.com/labstack/echo"
+)
 
+type res struct {
+	Status int         `json:"status"`
+	Data   interface{} `json:"data"`
+	Error  interface{} `json:"error"`
+}
+
+func Response(httpStatus int, data interface{}, error interface{}, ctx echo.Context) error {
+	payload := res{Status: httpStatus, Data: data, Error: error}
+	return ctx.JSON(httpStatus, payload)
 }
