18bc77b4a5af8ee1329b6d68a6bd565bc9eac763
geraldsamosir <gerald.unforgiven@gmail.com>
2020-12-03 18:42:03 +0700

    implemented client router serve wirh echo and implemented api for fullstack project

--- helper/response.go
+++ helper/response.go
@@ -10,6 +10,10 @@ type res struct {
 	Error  interface{} `json:"error"`
 }
 
+type DefaultMessage struct {
+	Message string `json:"message"`
+}
+
 func Response(httpStatus int, data interface{}, error interface{}, ctx echo.Context) error {
 	payload := res{Status: httpStatus, Data: data, Error: error}
 	return ctx.JSON(httpStatus, payload)
