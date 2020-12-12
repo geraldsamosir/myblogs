8d9e19c4ef28ea0c1183a9f283f1c94206636561
geraldsamosir <gerald.unforgiven@gmail.com>
2020-12-12 22:27:36 +0700

    add swagger

--- interface/webserver/server.go
+++ interface/webserver/server.go
@@ -25,21 +25,6 @@ import (
 type Webserver struct {
 }
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host petstore.swagger.io
-// @BasePath /api
-
 func (ws *Webserver) RunWebserver(db *gorm.DB) {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
