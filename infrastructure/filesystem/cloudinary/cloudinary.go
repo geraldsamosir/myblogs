d35b038ddd488670a20d4fec618e6235133e9c66
geraldsamosir <gerald.unforgiven@gmail.com>
2020-12-12 18:22:58 +0700

    implement gorutine for paralel upload to cloudinary

--- infrastructure/filesystem/cloudinary/cloudinary.go
+++ infrastructure/filesystem/cloudinary/cloudinary.go
@@ -1,8 +1,11 @@
 package cloudinary
 
 import (
+	"errors"
 	"fmt"
 	"mime/multipart"
+	"strings"
+	"sync"
 
 	"github.com/geraldsamosir/myblogs/domain"
 	"github.com/komfy/cloudinary"
@@ -19,20 +22,47 @@ func NewCloudinary(cs *cloudinary.Service) domain.FileUsecase {
 
 func (h UploadHandler) UploadMultipleFiles(files []*multipart.FileHeader) ([]string, error) {
 	urls := []string{}
-	for _, file := range files {
-		fmt.Println(file.Filename)
-		data, err := file.Open()
+	errs := []string{}
+	urlSuccess := make(chan string)
+	urlErrors := make(chan interface{})
+	var wg sync.WaitGroup
 
-		if err == nil {
-			upResp, err := h.cs.Upload(file.Filename, data, true)
-			if err != nil {
-				logrus.Error("sini err upload", err)
-				return nil, err
-			} else {
-				urls = append(urls, upResp.URL)
-			}
-		}
-		data.Close()
+	for _, file := range files {
+		wg.Add(1)
+		go h.paralelUploads(file, urlSuccess, urlErrors, &wg)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(urlErrors)
+		close(urlSuccess)
+	}()
+	for url := range urlSuccess {
+		urls = append(urls, url)
+	}
+	for err := range urlErrors {
+		errs = append(errs, fmt.Sprintf("%v", err))
+	}
+	if len(errs) > 0 {
+		return nil, errors.New(strings.Join(errs, " "))
+
 	}
 	return urls, nil
+}
+
+func (h UploadHandler) paralelUploads(file *multipart.FileHeader, success chan string, errors chan interface{}, wg *sync.WaitGroup) {
+	defer (*wg).Done()
+	logrus.Info(file.Filename)
+	data, err := file.Open()
+	if err == nil {
+		upResp, err := h.cs.Upload(file.Filename, data, true)
+		if err != nil {
+			logrus.Error("sini err upload", err)
+			errors <- err
+		} else {
+			success <- upResp.URL
+		}
+	}
+	data.Close()
 }
